9d11443a12c0040ab9fcda82611ad485a99263ce
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-04 15:16:42 +0200

    Removed debug from setup script

--- setup_ubuntu_18.04.sh
+++ setup_ubuntu_18.04.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# configuration variables
-dbg=0
-
 # SDK prerequisites
-sudo apt-get install \
+sudo apt-get install -y \
     build-essential\
     ocaml\
     ocamlbuild\
@@ -17,7 +14,7 @@ sudo apt-get install \
     git
 
 # PSW prerequisites
-sudo apt-get install\
+sudo apt-get install -y \
     libssl-dev\
     libcurl4-openssl-dev\
     protobuf-compiler\
@@ -35,14 +32,14 @@ sudo bash ./sgx_linux_x64_driver_2.6.0_51c4821.bin
 ./download_prebuilt.sh
 
 # build and install SDK
-make sdk DEBUG=$dbg
-make sdk_install_pkg DEBUG=$dbg
+make sdk
+make sdk_install_pkg
 bash linux/installer/bin/sgx_linux_x64_sdk_2.8.100.3.bin -prefix ~
 echo 'source ~/sgxsdk/environment' >> ~/.bashrc
 source ~/sgxsdk/environment
 
 # build PSW and .deb files
-make psw DEBUG=$dbg
-make deb_psw_pkg DEBUG=$dbg
+make psw
+make deb_psw_pkg
 
 sudo dpkg -i linux/installer/deb/**/*.deb
