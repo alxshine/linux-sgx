7329f9c1fc0784a8eb44668f00659ab61f8494bb
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-05 21:36:44 +0200

    Added driver as submodule

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -24,9 +24,8 @@ sudo apt-get install -y \
     reprepro
 
 # download and install driver
-wget https://download.01.org/intel-sgx/sgx-linux/2.8/distro/ubuntu18.04-server/sgx_linux_x64_driver_2.6.0_51c4821.bin
-# TODO: add SHA-256 of driver binary
-sudo bash ./sgx_linux_x64_driver_2.6.0_51c4821.bin
+git submodule update --init --recursive
+(cd driver && ./install-ubuntu.sh)
 
 # download prebuilt binaries
 ./download_prebuilt.sh
